103ef90a99715ce93d8117e9a0a33d807b5b7863
kfalencik <kfalencik@gmail.com>
2020-06-17 15:37:04 +0100

    Filter results by offices and/or managers

--- src/views/Employees.vue
+++ src/views/Employees.vue
@@ -2,24 +2,48 @@
   <v-card class="employees">
     <v-card-title>
       Employees
-      <v-spacer></v-spacer>
-      <v-text-field
-        v-model="search"
-        append-icon="mdi-magnify"
-        label="Search"
-        single-line
-        hide-details
-      ></v-text-field>
     </v-card-title>
 
     <v-data-table
-      class="pa-5"
       :headers="headers"
       :items="employees"
       :items-per-page="10"
-      :search="search"
       :loading="!employees.length"
+      item-key="id"
+      class="pa-5"
     >
+      <template v-slot:top>
+        <v-container fluid>
+          <v-row>
+            <v-col cols="6">
+              <v-row class="pa-6">
+                <v-select
+                  :items="managers"
+                  :clearable="true"
+                  filled
+                  label="Manager"
+                  item-text="fullName"
+                  item-value="id"
+                  v-model="managersFilterValue"
+                ></v-select>
+              </v-row>
+            </v-col>
+
+            <v-col cols="6">
+              <v-row class="pa-6">
+                <v-select
+                  :items="offices"
+                  :clearable="true"
+                  filled
+                  label="Site (Office/Location)"
+                  v-model="officesFilterValue"
+                ></v-select>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-container>
+      </template>
+
       <template v-show="!employees.length" v-slot:progress>
         <v-skeleton-loader v-for="row in 10" :key="`table-row-${row}`" class="mx-auto" type="table-row" />
       </template>
@@ -52,19 +76,53 @@ export default {
         { text: '', value: 'avatarUrl', sortable: false },
         { text: 'Full Name', value: 'fullName' },
         { text: 'Company', value: 'company', sortable: false },
-        { text: 'Site (Office/Location)', value: 'office' },
+        { text: 'Site (Office/Location)', value: 'office', filter: this.officesFilter },
+        { text: 'Manager', value: 'managerId', filter: this.managersFilter },
         { text: 'Email', value: 'email', sortable: false },
         { text: 'Date of Birth', value: 'dateOfBirth' },
-        { text: 'Start Date', value: 'startDate' },
-        { text: 'Manager', value: 'managerId' }
+        { text: 'Start Date', value: 'startDate' }
       ],
-      search: ''
+      managersFilterValue: null,
+      officesFilterValue: null
     }
   },
   computed: {
     employees () {
       // Get employees data
       return this.$store.state.employees
+    },
+    managers () {
+      // Get manager IDs
+      let managers = this.employees.map(item => item.managerId)
+
+      // Remove null values
+      managers = managers.filter(item => item !== null)
+
+      // Use sets to get unique manager IDs
+      managers = new Set(...[managers])
+
+      // Turn into objects with manager data
+      managers = [...managers].map(id => {
+        const manager = {
+          id,
+          fullName: this.managerFullName(id)
+        }
+        return manager
+      })
+
+      return managers
+    },
+    offices () {
+      // Get offices data
+      let offices = this.employees.map(item => item.office)
+
+      // Remove null values
+      offices = offices.filter(item => item !== null)
+
+      // Use sets to get unique offices
+      offices = new Set(...[offices])
+
+      return [...offices]
     }
   },
   methods: {
@@ -73,6 +131,24 @@ export default {
       const manager = this.employees.filter(item => item.id === id)
       // Return managers full name
       return manager.length ? manager[0].fullName : '-'
+    },
+    managersFilter (value) {
+      // If this filter has no value we just skip the entire filter.
+      if (!this.managersFilterValue) {
+        return true
+      }
+      // Check if the current loop value (The managerId value)
+      // equals to the selected value at the <v-select>.
+      return value === this.managersFilterValue
+    },
+    officesFilter (value) {
+      // If this filter has no value we just skip the entire filter.
+      if (!this.officesFilterValue) {
+        return true
+      }
+      // Check if the current loop value (The office value)
+      // equals to the selected value at the <v-select>.
+      return value === this.officesFilterValue
     }
   }
 }
