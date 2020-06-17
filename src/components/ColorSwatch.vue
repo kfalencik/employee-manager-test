67d9329e8749d51dd797d70e0e2f8178b2242dca
kfalencik <kfalencik@gmail.com>
2020-06-17 19:16:41 +0100

    New color swatch component

---
+++ src/components/ColorSwatch.vue
@@ -0,0 +1,16 @@
+<template>
+  <span class="color-swatch d-inline-block" :style="`background-color: ${color};`"></span>
+</template>
+
+<style lang="scss" scoped>
+  .color-swatch {
+    width: 13px;
+    height: 13px;
+  }
+</style>
+
+<script>
+export default {
+  props: ['color']
+}
+</script>
