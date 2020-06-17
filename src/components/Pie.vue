5819becce0ee1aa15007544cfb6b71d95c2f4bd2
kfalencik <kfalencik@gmail.com>
2020-06-17 17:40:30 +0100

    Chart pie component

---
+++ src/components/Pie.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="pie" v-if="data">
+    <PiePiece v-for="(piece, index) in data"
+      :key="`pie-piece-${index}`"
+      :rotate="piece.rotate"
+      :size="pieSize"
+      :value="piece.value"
+      :width="pieWidth"
+      :color="piece.color"
+    />
+  </div>
+</template>
+
+<script>
+import PiePiece from '@/components/PiePiece.vue'
+
+export default {
+  props: ['data'],
+  data () {
+    return {
+      pieSize: 400,
+      pieWidth: 200
+    }
+  },
+  components: {
+    PiePiece
+  }
+}
+</script>
---
+++ src/components/PiePiece.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-progress-circular
+    class="pie-piece"
+    :rotate="rotate"
+    :size="size"
+    :value="value"
+    :width="width"
+    :color="color"
+  />
+</template>
+
+<script>
+export default {
+  props: [
+    'rotate',
+    'size',
+    'value',
+    'width',
+    'color'
+  ]
+}
+</script>
+
+<style lang="scss">
+  .pie-piece {
+    position: absolute;
+  }
+
+  .v-progress-circular__underlay {
+    stroke: none;
+  }
+</style>
