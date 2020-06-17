198c70156e7ad3e817fa7bb138b3dcb341e27fc7
kfalencik <kfalencik@gmail.com>
2020-06-17 14:49:51 +0100

    Employee table with correct fields

--- src/views/Employees.vue
+++ src/views/Employees.vue
@@ -13,15 +13,31 @@
     </v-card-title>
 
     <v-data-table
+      class="pa-5"
       :headers="headers"
       :items="employees"
       :items-per-page="10"
       :search="search"
       :loading="!employees.length"
-      class="elevation-1"
     >
       <template v-show="!employees.length" v-slot:progress>
         <v-skeleton-loader v-for="row in 10" :key="`table-row-${row}`" class="mx-auto" type="table-row" />
+      </template>
+
+      <template v-slot:item.dateOfBirth="{ item }">
+        {{ item.dateOfBirth | dateFormatter }}
+      </template>
+
+      <template v-slot:item.startDate="{ item }">
+        {{ item.startDate | dateFormatter }}
+      </template>
+
+      <template v-slot:item.managerId="{ item }">
+        {{ managerFullName(item.managerId) }}
+      </template>
+
+      <template v-slot:item.avatarUrl="{ item }">
+        <v-img height="48" width="48" :src="item.avatarUrl" />
       </template>
     </v-data-table>
   </v-card>
@@ -33,18 +49,30 @@ export default {
   data () {
     return {
       headers: [
-        { text: 'First name', value: 'name.first' },
-        { text: 'Last name', value: 'name.last' },
-        { text: 'Company', value: 'company' },
-        { text: 'Office', value: 'office' },
-        { text: 'Email', value: 'email' }
+        { text: '', value: 'avatarUrl', sortable: false },
+        { text: 'Full Name', value: 'fullName' },
+        { text: 'Company', value: 'company', sortable: false },
+        { text: 'Site (Office/Location)', value: 'office' },
+        { text: 'Email', value: 'email', sortable: false },
+        { text: 'Date of Birth', value: 'dateOfBirth' },
+        { text: 'Start Date', value: 'startDate' },
+        { text: 'Manager', value: 'managerId' }
       ],
       search: ''
     }
   },
   computed: {
     employees () {
+      // Get employees data
       return this.$store.state.employees
+    }
+  },
+  methods: {
+    managerFullName (id) {
+      // Find manager within employees array
+      const manager = this.employees.filter(item => item.id === id)
+      // Return managers full name
+      return manager.length ? manager[0].fullName : '-'
     }
   }
 }
