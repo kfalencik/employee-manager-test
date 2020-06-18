f7f60d28badb584a7098f1f06da317455a5bb1a7
kfalencik <kfalencik@gmail.com>
2020-06-18 16:04:04 +0100

    hotfix

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div app class="d-flex fill-height" :class="{'flex-column': $vuetify.breakpoint.smAndDown}">
       <Navigation app />
 
-      <v-main :style="{'width': $vuetify.breakpoint.mdAndUp ? 'calc(100% - 300px)' : '100%'}">
+      <v-main app :style="{'width': $vuetify.breakpoint.mdAndUp ? 'calc(100% - 300px)' : '100%'}">
         <div class="pa-4 py-md-8 px-md-7 py-lg-8 px-lg-10">
           <template v-if="message">
             <Message :message="message.message" :type="message.type" />
